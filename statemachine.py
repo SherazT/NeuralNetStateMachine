5c2c68363005c5f20c9bc7882977bc43a41ea425
Sheraz Tariq <sheraztariq@hotmail.com>
2016-09-07 00:02:57 -0400

    predicts accurately next state

--- statemachine.py
+++ statemachine.py
@@ -19,5 +19,30 @@ def sigmoid(x,deriv=False):
 	return 1/(1+np.exp(-x))
 Xaxis = np.arange(-6,6, 0.1)
 
-plt.plot(Xaxis, sigmoid(Xaxis))
-plt.show()
+# plt.plot(Xaxis, sigmoid(Xaxis))
+# plt.show()
+
+# seed random numbers 
+np.random.seed(1)
+
+# initialize weights randomly with mean 0
+syn0 = 2*np.random.random((3,1)) - 1
+
+for iter in xrange(10000):
+
+    # forward propagation
+    l0 = x
+    l1 = sigmoid(np.dot(l0,syn0))
+
+    # how much did we miss?
+    l1_error = y - l1
+
+    # multiply how much we missed by the 
+    # slope of the sigmoid at the values in l1
+    l1_delta = l1_error * sigmoid(l1,True)
+
+    # update weights
+    syn0 += np.dot(l0.T,l1_delta)
+
+print "Output After Training:"
+print l1
